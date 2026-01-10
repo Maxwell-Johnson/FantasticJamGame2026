e4183f63b95a2927cd3ae18e0f4bef99c39f6f74
Maxwell Johnson <Maxwell.m.johnson10@gmail.com>
2026-01-10 17:30:34 -0500

    Added an Enemy Movement Script

--- Assets/Scripts/Enemy Scripts/Enemy_Properties.cs
+++ Assets/Scripts/Enemy Scripts/Enemy_Properties.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class Enemy_Properties : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float maxHealth = 1f;
+    [SerializeField] public float maxHealth = 1f;
     private float currentHealth;
+
+    private SpriteRenderer sprite;
+    private bool isTakingDamage = false;
+    private float damageTimer;
+    private float damageInvulnerabilityTime = 0.5f;
 
 
     private void Awake()
     {
+        sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
     void Start()
@@ -18,7 +24,36 @@ public class Enemy_Properties : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        isTakingDamage = true;
         currentHealth -= damage;
         if (currentHealth <= 0) Destroy(gameObject); //if health 0, dead
+        while (isTakingDamage)
+        {
+            sprite.color = Color.black;
+            sprite.color = Color.white;
+        }
+
+    }
+
+    private void Update()
+    {
+        CheckTakingDamageTimer();
+    }
+
+    void CheckTakingDamageTimer()
+    {
+
+        
+        if (isTakingDamage)
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= damageInvulnerabilityTime)
+            {
+                damageTimer = 0;
+                isTakingDamage = false;
+                
+            }
+        }
+
     }
 }
